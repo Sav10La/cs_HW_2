2e9307f72b917b06eb0d1fbd6fd17211c2b6717b
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-06-29 18:55:35 +0500

    add Extra04 MaxMinIndex

---
+++ Extra04/Program.cs
@@ -0,0 +1,35 @@
+﻿// Дополнительные задачи.
+// Задача 4. Дано натуральное число, в котором все цифры различны. Определить, какая цифра расположена в нем левее: максимальная или минимальная.
+
+long a = 1076543289;
+string b = Convert.ToString(a);
+char[] array = b.ToCharArray();
+int index = 0;
+int length = array.Length;
+int max = array[0];
+int min = array[0];
+int imax = 0;
+int imin = 0;
+
+while (index < length)
+{
+    if (array[index]>max)
+    {
+        max = array[index];
+        imax = index;        
+    }
+    else if (array[index]<min)
+    {
+        min = array[index];
+        imin = index;
+    }
+    index++;
+}
+if (imax > imin)
+{
+    Console.WriteLine("Левее находится минимальная цифра.");
+}
+else
+{
+    Console.WriteLine("Левее находится максимальная цифра.");
+}
