24cb7e53f8327a4611e2f5cda3a2f646ec2f84ea
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-06-29 12:38:35 +0500

    add Extra01 Triangle

---
+++ Extra01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Дополнительные задачи.
+//Задача 1. Написать программу, которая определяет, является ли треугольник со сторонами a, b, c равнобедренным.
+
+Console.WriteLine("Введите длину стороны a:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину стороны b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину стороны c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Triangle(a, b, c);
+
+void Triangle(int a, int b, int c)
+{
+    if (a == b | b == c | a == c)
+    {
+        Console.WriteLine("Треугольник со сторонами a, b, c является равнобедренным");
+    }
+    else
+    {
+        Console.WriteLine("Треугольник со сторонами a, b, c не является равнобедренным");
+    }
+}
+Console.WriteLine();
